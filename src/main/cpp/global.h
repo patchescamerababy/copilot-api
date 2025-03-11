16531655b0601a81b6045cae7c04f76144c07f4f
patchescamerababy <116485523+patchescamerababy@users.noreply.github.com>
2025-03-11 21:25:13 +0800

    Delete src/main/cpp directory

--- src/main/cpp/global.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-
-#include "header_manager.h"
-
-// 获取HeaderManager单例实例的全局函数
-HeaderManager& getHeaderManager();
-
-#endif // GLOBAL_H
--- src/main/cpp/token_manager.h
+++
@@ -1,9 +0,0 @@
-#ifndef TOKEN_MANAGER_H
-#define TOKEN_MANAGER_H
-
-#include <string>
-
-// 声明获取有效临时 Token 的函数
-std::string getValidTempToken(const std::string &longTermToken);
-
-#endif // TOKEN_MANAGER_H
