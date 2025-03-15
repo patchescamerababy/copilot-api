9d3b087ccaa096fbcbd0139cfdff64f1d7dc4836
patchescamerababy <116485523+patchescamerababy@users.noreply.github.com>
2025-03-15 20:09:14 +0800

    Create SqliteJdbcFeature.java

---
+++ src/org/sqlite/nativeimage/SqliteJdbcFeature.java
@@ -0,0 +1,10 @@
+package org.sqlite.nativeimage;
+
+import org.graalvm.nativeimage.hosted.Feature;
+
+public class SqliteJdbcFeature implements Feature {
+    @Override
+    public void beforeAnalysis(BeforeAnalysisAccess access) {
+        // Do Nothing
+    }
+}
